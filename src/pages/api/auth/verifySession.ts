d3d90d2e077ea8fda18fae1e1a2521c2cdf3a141
brian-gee <39035211+brian-gee@users.noreply.github.com>
2023-10-23 20:51:44 -0400

    adding styles and updating api

--- src/pages/api/auth/verifySession.ts
+++ src/pages/api/auth/verifySession.ts
@@ -2,30 +2,23 @@ import type { APIRoute } from 'astro';
 import { app } from '../../../firebase/server';
 import { getAuth } from 'firebase-admin/auth';
 
-export const get: APIRoute = async ({ request, cookies }) => {
+export const GET: APIRoute = async ({ request, cookies }) => {
   const auth = getAuth(app);
 
   // Get the session cookie value from the request
   const sessionCookie = cookies.get('session')?.value;
 
   if (!sessionCookie) {
-    return {
-      status: 401,
-      body: 'No session cookie found',
-    };
+    return new Response('No session cookie found', { status: 401 });
   }
 
   // Verify the session cookie
   try {
     const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
-    return {
+    return new Response(JSON.stringify({ uid: decodedClaims.uid }), {
       status: 200,
-      body: JSON.stringify({ uid: decodedClaims.uid }),
-    };
+    });
   } catch (error) {
-    return {
-      status: 401,
-      body: 'Invalid session cookie',
-    };
+    return new Response('Invalid session cookie', { status: 401 });
   }
 };
